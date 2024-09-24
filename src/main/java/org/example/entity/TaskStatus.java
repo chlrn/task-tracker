973b9c28e085ca415364439d1547b0246cfe72e6
chlrn <104934124+chlrn@users.noreply.github.com>
2024-09-24 18:45:56 +0400

    Delete src directory

--- src/main/java/org/example/entity/TaskStatus.java
+++
@@ -1,9 +0,0 @@
-package org.example.entity;
-
-public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    REVIEW,
-    TEST,
-    DONE
-}
--- src/main/java/org/example/exception/InvalidTaskStatusTransitionException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class InvalidTaskStatusTransitionException extends RuntimeException {
-    public InvalidTaskStatusTransitionException(String message) {
-        super(message);
-    }
-}
